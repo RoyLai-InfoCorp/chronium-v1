af1c0d6268ec9b24abbba3ddf6c0847e0999eca6
RoyLai-InfoCorp <roy.lai@infocorp.io>
2023-06-18 09:50:04 +0800

    updated hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,50 +4,51 @@ require("@nomiclabs/hardhat-etherscan");
 require("dotenv").config();
 
 module.exports = {
-  solidity: {
-    version: "0.8.8",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 1000,
-      },
+    defaultNetwork: "hardhat",
+    solidity: {
+        version: "0.8.8",
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 1000,
+            },
+        },
     },
-  },
-  paths: {
-    sources: "./contracts",
-    artifacts: "./build",
-  },
-  mocha: {
-    recursive: true,
-    timeout: "40000",
-  },
-  networks: {
-    hardhat: {
-      chainId: 1337,
-      accounts: {
-        mnemonic: process.env.CHRONIUM_LOCAL__MNEMONIC,
-      },
+    paths: {
+        sources: "./contracts",
+        artifacts: "./build",
     },
-    goerli: {
-      chainId: 5,
-      url: process.env.CHRONIUM_GOERLI__URL,
-      accounts: {
-        mnemonic: process.env.CHRONIUM_GOERLI__MNEMONIC,
-      },
+    mocha: {
+        recursive: true,
+        timeout: "40000",
     },
-    mainnet: {
-      chainId: 1,
-      url: process.env.CHRONIUM_MAINNET__URL,
-      accounts: {
-        mnemonic: process.env.CHRONIUM_MAINNET__MNEMONIC,
-        passphrase: process.env.CHRONIUM_MAINNET__PASSPHRASE,
-      },
+    networks: {
+        hardhat: {
+            chainId: 1337,
+            accounts: {
+                mnemonic: process.env.CHRONIUM_LOCAL__MNEMONIC,
+            },
+        },
+        goerli: {
+            chainId: 5,
+            url: process.env.CHRONIUM_TESTNET__URL,
+            accounts: {
+                mnemonic: process.env.CHRONIUM_TESTNET__MNEMONIC,
+            },
+        },
+        mainnet: {
+            chainId: 1,
+            url: process.env.CHRONIUM_MAINNET__URL,
+            accounts: {
+                mnemonic: process.env.CHRONIUM_MAINNET__MNEMONIC,
+                passphrase: process.env.CHRONIUM_MAINNET__PASSPHRASE,
+            },
+        },
     },
-  },
-  etherscan: {
-    apiKey: {
-      goerli: process.env.CHRONIUM_ETHERSCAN,
-      mainnet: process.env.CHRONIUM_ETHERSCAN
+    etherscan: {
+        apiKey: {
+            goerli: process.env.CHRONIUM_ETHERSCAN,
+            mainnet: process.env.CHRONIUM_ETHERSCAN,
+        },
     },
-  },
 };
